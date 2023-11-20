f3ac00431acb84673f23fc7dc80149f48c2b3b31
Kim Jiwon <stayby16@kakao.com>
2023-11-20 17:18:28 +0900

    🔥firebase project 초기설정

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQW_2AhNnHiCC8Dqfc_IJhXtYrBVZveYQ",
+  authDomain: "twitter-clone-c7902.firebaseapp.com",
+  projectId: "twitter-clone-c7902",
+  storageBucket: "twitter-clone-c7902.appspot.com",
+  messagingSenderId: "231979696442",
+  appId: "1:231979696442:web:04788bc50cf47de1da1003"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
