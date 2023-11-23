6969519b31d5db68f2c3405743e56bff049180e1
Kim Jiwon <stayby16@kakao.com>
2023-11-23 20:55:43 +0900

    🐛 로그아웃 시 라우팅 추가

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom"
 import { auth } from "../firebase"
 
 function Home() {
+  const navigate = useNavigate()
   const logOut = ()=>{
     // 로그아웃 기능 구현 시에는 firebase 파일에서 생성한 auth 인스턴스를 호출한 다음 signOut만 하면 된다.
     auth.signOut()
+    navigate("/login")
   }
   return (
     <h1>
