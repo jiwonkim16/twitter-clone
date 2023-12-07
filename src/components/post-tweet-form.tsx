dd19ea8fb2dea9fa780bce7574536f54b5706c31
Kim Jiwon <stayby16@kakao.com>
2023-12-07 21:50:24 +0900

    ⚡️ Tweeting to FireStore

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,79 +1,119 @@
-import { useState } from "react"
-import styled from "styled-components"
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
+import styled from "styled-components";
+import { auth, db } from "../firebase";
 
 const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 const TextArea = styled.textarea`
-    border: 2px solid white;
-    padding: 20px;
-    border-radius: 20px;
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
     font-size: 16px;
-    color: white;
-    background-color: black;
-    width: 100%;
-    resize: none;
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    &::placeholder {
-        font-size: 16px;
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-    &:focus {
-        outline: none;
-        border-color: #1d9bf0;
-    }
-`
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
 const AttachFileButton = styled.label`
-    padding: 10px 0px;
-    color: #1d9bf0;
-    text-align: center;
-    border-radius: 20px;
-    font-size: 14px;
-    font-weight: 600;
-    cursor: pointer;
-    border: 1px solid #1d9bf0;
-`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+  border: 1px solid #1d9bf0;
+`;
 const AttachFileInput = styled.input`
-    display: none;
-`
+  display: none;
+`;
 const SubmitBtn = styled.input`
-    background-color: #1d9bf0;
-    color: white;
-    border: none;
-    padding: 10px 0px;
-    border-radius: 20px;
-    font-size: 16px;
-    cursor: pointer;
-    &:hover,
-    &:active {
-        opacity: 0.8;
-    } 
-`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.8;
+  }
+`;
 
 function PostTweetForm() {
-    const [isLoading, setIsLoading] = useState(false)
-    const [tweet, setTweet] = useState("") 
-    const [file, setFile] = useState<File | null>(null)
-    const onChange = (e:React.ChangeEvent<HTMLTextAreaElement>)=>{
-        setTweet(e.target.value)
+  const [isLoading, setIsLoading] = useState(false);
+  const [tweet, setTweet] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  };
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length === 1) {
+      setFile(files[0]);
     }
-    const onFileChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        const {files} = e.target
-        if(files &&  files.length === 1){
-            setFile(files[0])
-        }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+    try {
+      setIsLoading(true);
+      await addDoc(collection(db, "tweet"), {
+        tweet,
+        createdAt: Date.now(),
+        username: user.displayName || "Anonymous",
+        userId: user.uid,
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
     }
+  };
   return (
-    <Form>
-        <TextArea rows={5} maxLength={180} value={tweet} placeholder="What is happening" onChange={onChange} />
-        <AttachFileButton htmlFor="file">{file ? "Photo added ✅" : "Add photo"}</AttachFileButton>
-        {/* 이미지 파일을 받는데 어떤 확장자라도 상관없음 */}
-        <AttachFileInput id="file" type="file" accept="image/*" onChange={onFileChange} />
-        <SubmitBtn type="submit" value={isLoading ? "Posting..." : "Post Tweet"} />
+    <Form onSubmit={onSubmit}>
+      <TextArea
+        rows={5}
+        maxLength={180}
+        value={tweet}
+        placeholder="What is happening"
+        onChange={onChange}
+      />
+      <AttachFileButton htmlFor="file">
+        {file ? "Photo added ✅" : "Add photo"}
+      </AttachFileButton>
+      {/* 이미지 파일을 받는데 어떤 확장자라도 상관없음 */}
+      <AttachFileInput
+        id="file"
+        type="file"
+        accept="image/*"
+        onChange={onFileChange}
+      />
+      <SubmitBtn
+        type="submit"
+        value={isLoading ? "Posting..." : "Post Tweet"}
+      />
     </Form>
-  )
+  );
 }
 
-export default PostTweetForm
+export default PostTweetForm;
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDQW_2AhNnHiCC8Dqfc_IJhXtYrBVZveYQ",
@@ -7,7 +9,7 @@ const firebaseConfig = {
   projectId: "twitter-clone-c7902",
   storageBucket: "twitter-clone-c7902.appspot.com",
   messagingSenderId: "231979696442",
-  appId: "1:231979696442:web:04788bc50cf47de1da1003"
+  appId: "1:231979696442:web:04788bc50cf47de1da1003",
 };
 
 // Initialize Firebase
@@ -15,4 +17,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // 그 app에 대한 인증 서비스를 사용하고 싶다!
-export const auth = getAuth(app)
+export const auth = getAuth(app);
+
+// 데이터베이스와 스토리지에 대한 액세스 설정
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
