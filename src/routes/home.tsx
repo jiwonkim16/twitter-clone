f4864e9225ec7f513490e26d2716e96a69e8dd56
Kim Jiwon <stayby16@kakao.com>
2023-12-07 19:26:20 +0900

    ⚡️ Navigation bar

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,16 +1,6 @@
-import { useNavigate } from "react-router-dom"
-import { auth } from "../firebase"
-
 function Home() {
-  const navigate = useNavigate()
-  const logOut = ()=>{
-    // 로그아웃 기능 구현 시에는 firebase 파일에서 생성한 auth 인스턴스를 호출한 다음 signOut만 하면 된다.
-    auth.signOut()
-    navigate("/login")
-  }
   return (
     <h1>
-      <button onClick={logOut}>Log Out</button>
     </h1>
   )
 }
