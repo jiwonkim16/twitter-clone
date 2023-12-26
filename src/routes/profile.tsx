31b2cc62a681a7c0902d046131ad356144668256
Kim Jiwon <stayby16@kakao.com>
2023-12-26 21:39:17 +0900

    ✨ user Avatar

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,8 +1,93 @@
+import styled from "styled-components";
+import { auth, storage } from "../firebase";
+import { useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { updateProfile } from "firebase/auth";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #1d9bf0;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  svg {
+    width: 50px;
+  }
+`;
+const AvatarImg = styled.img`
+  width: 100%;
+`;
+const AvatarInput = styled.input`
+  display: none;
+`;
+const Name = styled.span`
+  font-size: 22px;
+`;
 
 function Profile() {
+  const user = auth.currentUser;
+  const [avatar, setAvatar] = useState(user?.photoURL || "");
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!user) return;
+    if (files && files.length === 1) {
+      const file = files[0];
+      // avatars라는 폴더에 유저ID로 사진을 업로드
+      // 유저 이미지를 저장할 수 있는 ref 생성
+      const locationRef = ref(storage, `avatars/${user?.uid}`);
+      // 이미지 업로드
+      const result = await uploadBytes(locationRef, file);
+      // 이미지의 URL을 얻고
+      const avatarUrl = await getDownloadURL(result.ref);
+      setAvatar(avatarUrl);
+      // 유저 프로필을 업데이트
+      await updateProfile(user, {
+        photoURL: avatarUrl,
+      });
+    }
+  };
   return (
-    <div>profile</div>
-  )
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {Boolean(avatar) ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            data-slot="icon"
+            fill="none"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+            />
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        onChange={onAvatarChange}
+        id="avatar"
+        type="file"
+        accept="image/*"
+      />
+      <Name>{user?.displayName ? user.displayName : "어나미머스"}</Name>
+    </Wrapper>
+  );
 }
 
-export default Profile
+export default Profile;
